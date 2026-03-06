dd384606b4223cbc9b71b9d3a5567109da458653
Upendra-3596 <gudariupendra9@example.com>
2026-03-06 11:39:31 +0530

    first commit

---
+++ src/main/java/com/pipline/jenkins/jenkinsController.java
@@ -0,0 +1,12 @@
+package com.pipline.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class jenkinsController {
+
+    @GetMapping("jenkins")
+    public String welcomeMessage() {
+        return "Welcome to Spring Boot Application";
+    }
+
+}
