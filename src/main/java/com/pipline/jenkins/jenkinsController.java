4e21e79b99edae9b90432638e4afaa77a36ba8c9
Upendra-3596 <gudariupendra9@example.com>
2026-03-06 18:27:47 +0530

    add k8s folder

--- src/main/java/com/pipline/jenkins/jenkinsController.java
+++ src/main/java/com/pipline/jenkins/jenkinsController.java
@@ -1,13 +1,17 @@
 package com.pipline.jenkins;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("jenkins") // This is the first part of the URL
 public class jenkinsController {
-
-    @GetMapping("jenkins")
+    @GetMapping("/welcome") // This is the second part
     public String welcomeMessage() {
-        return "Welcome to Spring Boot Application";
+        return "Welcome to jenkins Application";
     }
+}
     //added comment for maven
     //docker add
-}
+
